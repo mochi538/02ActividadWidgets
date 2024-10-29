d7dd1b6d7ac3f1f7954b8bc23da01a6b0ed29873
mochi538 <mariar53804@gmail.com>
2024-10-29 12:14:17 -0500

    commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyCustomForm());
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+class MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Form(
+      key: _formKey,
+      child: const Column(
+        children: <Widget>[
+          // Add TextFormFields and ElevatedButton here.
+        ],
+      ),
+    );
+  }
+}
